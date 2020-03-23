c9a6b45adf8b0ef6a21df43ac1ec9a89d5bdbc5c
工藤海斗 <kaitokudou2468110921@gmail.com>
2020-03-24 00:05:34 +0900

    UISearchBarとUITableViewを配置して、リクエストURLを生成

--- SearchOkashi/SearchOkashi/ViewController.swift
+++ SearchOkashi/SearchOkashi/ViewController.swift
@@ -9,13 +9,45 @@
 import UIKit
 import Alamofire
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UISearchBarDelegate {
 
+    @IBOutlet weak var searchText: UISearchBar!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        searchText.delegate = self
+        searchText.placeholder = "お菓子の名前を入力してください"
     }
-
+    
+    // 検索ボタンクリック時に呼ばれるdelegateメソッド
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        // キーボードを閉じる
+        view.endEditing(true)
+        
+        guard let searchWord = searchBar.text else {
+            return
+        }
+        print(searchWord)
+        searchOkashi(keyword: searchWord)
+    }
+    
+    // 引数keywordはUISearchBarに入力する検索したいキーワード
+    func searchOkashi(keyword : String){
+        // 全角文字をエンコードする
+        guard let keywordEncode = keyword.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
+            return
+        }
+        
+        // リクエストURL生成
+        guard let requestURL = URL(string: "https://sysbird.jp/toriko/api/?apikey=guest&format=json&keyword=\(keywordEncode)&max=10&order=r") else {
+            return
+        }
+        print(requestURL)
+    }
 
 }
 
