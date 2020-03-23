6b2e4e0eb7b3ac8075f6835ffcf42e6d35d021a3
工藤海斗 <kaitokudou2468110921@gmail.com>
2020-03-23 23:04:16 +0900

    CocoaPodsを使ってAlamofireを導入

---
+++ SearchOkashi/Pods/Target Support Files/Pods-SearchOkashi/Pods-SearchOkashi-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SearchOkashi : NSObject
+@end
+@implementation PodsDummy_Pods_SearchOkashi
+@end
---
+++ SearchOkashi/SearchOkashi/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SearchOkashi
+//
+//  Created by 工藤海斗 on 2020/03/23.
+//  Copyright © 2020 工藤海斗. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
